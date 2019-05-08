dab3dbaa237fc9328393670fe59935968758fd5a
frahman5 <faiyam@faiyamrahman.com>
2019-05-07 23:09:06 -0400

    add react app in

--- services/publicapi/publicapi.go
+++ services/publicapi/publicapi.go
@@ -125,6 +125,10 @@ func (a *API) HandleDetectLanguage(w http.ResponseWriter, r *http.Request) {
 		}
 	}
 
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+	w.Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization")
+
 	fmt.Fprint(w, guessTag)
 }
 
